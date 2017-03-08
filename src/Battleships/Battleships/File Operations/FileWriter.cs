a36f06312a75145eff9c31631396e642b239c347
TRIFECTA\Kim <kim.scicluna.a106280@mcast.edu.mt>
2017-03-08 20:08:17 +0100

    Fixed Directory. Created Battleships csharp project.

---
+++ src/Battleships/Battleships/File Operations/FileWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.File_Operations
+{
+    class FileWriter : Files
+    {
+        public static FileEnum Write(string path, string content)
+        {
+            try
+            {
+                if (File.Exists(path))
+                    return FileEnum.WriteFailed;
+
+                File.WriteAllText(path, content);
+                return FileEnum.WriteSuccessful;
+            }
+            catch
+            {
+                return FileEnum.WriteFailed;
+            }
+        }
+    }
+}
---
+++ src/Battleships/Battleships/File Operations/Files.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.File_Operations
+{
+    class Files
+    {
+        public enum FileEnum
+        {
+            WriteSuccessful = 0,
+            WriteFailed = 1,
+            ReadFailed = 2
+        }
+    }
+}
